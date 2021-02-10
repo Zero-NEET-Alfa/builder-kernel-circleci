c8bb516b8689d8d8b3ba59c448684613bb2615b8
Zero-NEET-Alfa <danidaboy54@gmail.com>
2021-02-10 18:11:34 +0700

    update builder clang

--- do-bn-u.sh
+++ do-bn-u.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 branch="20201208/BN-U"
-BuilderKernel="00000"
+BuilderKernel="clang"
 
 if [ "$BuilderKernel" != "clang" ] && [ "$BuilderKernel" != "dtc" ] && [ "$BuilderKernel" != "gcc" ] ;then
     exit;
